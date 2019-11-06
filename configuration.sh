1b8dee3d169ef9487ee44deab29f868ca4c38af4
Yunkon(Alvin) Kim <hermitkim1@gmail.com>
2019-11-06 13:26:45 +0900

    First commit
    - Make a script

---
+++ configuration.sh
@@ -0,0 +1,12 @@
+# This is Alvin's script for 
+# default configuration of Raspberry Pi.
+
+# system update
+sudo apt update
+sudo apt upgrade
+
+# install vim
+sudo apt install vim
+
+# install Nanum fonts for Korean
+sudo apt install fonts-nanum fonts-nanum-extra
